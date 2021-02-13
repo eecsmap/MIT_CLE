0a4378c6d0c3083ace4079338a915cb8533137de
philoinovsky <1129410550@qq.com>
2021-02-14 00:00:21 +0800

    updated at Feb 14 2021

--- Compiler/src/edu/mit/compilers/Main.java
+++ Compiler/src/edu/mit/compilers/Main.java
@@ -3,7 +3,10 @@ package edu.mit.compilers;
 import java.io.*;
 import java.text.DecimalFormat;
 
+import antlr.CharStreamException;
+import antlr.RecognitionException;
 import antlr.Token;
+import antlr.TokenStreamException;
 import edu.mit.compilers.grammar.*;
 import edu.mit.compilers.tools.CLI;
 import edu.mit.compilers.tools.CLI.Action;
@@ -16,64 +19,72 @@ class Main {
           System.in : new java.io.FileInputStream(CLI.infile);
       PrintStream outputStream = CLI.outfile == null ? System.out : new java.io.PrintStream(new java.io.FileOutputStream(CLI.outfile));
       if (CLI.target == Action.SCAN) {
-        DecafScanner scanner =
-            new DecafScanner(new DataInputStream(inputStream));
-        scanner.setTrace(CLI.debug);
-        Token token;
-        boolean done = false;
-        while (!done) {
-          try {
-            for (token = scanner.nextToken();
-                 token.getType() != DecafParserTokenTypes.EOF;
-                 token = scanner.nextToken()) {
-              String type = "";
-              String text = token.getText();
-              switch (token.getType()) {
-               // TODO: add strings for the other types here...
-                case DecafScannerTokenTypes.CHARLITERAL:
-                  type = " CHARLITERAL";
-                  break;
-                case DecafScannerTokenTypes.INTLITERAL:
-                  type = " INTLITERAL";
-                  break;
-                case DecafScannerTokenTypes.STRINGLITERAL:
-                  type = " STRINGLITERAL";
-                  break;
-                case DecafScannerTokenTypes.ID:
-                  type = " IDENTIFIER";
-                  break;
-                case DecafScannerTokenTypes.TK_false:
-                case DecafScannerTokenTypes.TK_true:
-                  type = " BOOLEANLITERAL";
-                  break;
-                default:
-                  type = Integer.toString(token.getType());
-                  type = "";
-                  break;
-              }
-              outputStream.println(token.getLine() + type + " " + text);
-            }
-            done = true;
-          } catch(Exception e) {
-            // print the error:
-            System.err.println(CLI.infile + " " + e);
-            scanner.consume();
-          }
-        }
+        scan(inputStream, outputStream);
       } else if (CLI.target == Action.PARSE ||
                  CLI.target == Action.DEFAULT) {
-        DecafScanner scanner =
-            new DecafScanner(new DataInputStream(inputStream));
-        DecafParser parser = new DecafParser(scanner);
-        parser.setTrace(CLI.debug);
-        parser.program();
-        if(parser.getError()) {
-          System.exit(1);
-        }
+        parse(inputStream, outputStream);
+      } else if (CLI.target == Action.INTER) {
+        
       }
     } catch(Exception e) {
       // print the error:
       System.err.println(CLI.infile+" "+e);
+    }
+  }
+
+  private static void scan(InputStream inputStream, PrintStream outputStream) throws CharStreamException {
+    DecafScanner scanner =
+    new DecafScanner(new DataInputStream(inputStream));
+    scanner.setTrace(CLI.debug);
+    Token token;
+    boolean done = false;
+    while (!done) {
+      try {
+        for (token = scanner.nextToken();
+            token.getType() != DecafParserTokenTypes.EOF;
+            token = scanner.nextToken()) {
+          String type = "";
+          String text = token.getText();
+          switch (token.getType()) {
+            case DecafScannerTokenTypes.CHARLITERAL:
+              type = " CHARLITERAL";
+              break;
+            case DecafScannerTokenTypes.INTLITERAL:
+              type = " INTLITERAL";
+              break;
+            case DecafScannerTokenTypes.STRINGLITERAL:
+              type = " STRINGLITERAL";
+              break;
+            case DecafScannerTokenTypes.ID:
+              type = " IDENTIFIER";
+              break;
+            case DecafScannerTokenTypes.TK_false:
+            case DecafScannerTokenTypes.TK_true:
+              type = " BOOLEANLITERAL";
+              break;
+            default:
+              type = Integer.toString(token.getType());
+              type = "";
+              break;
+          }
+          outputStream.println(token.getLine() + type + " " + text);
+        }
+        done = true;
+      } catch(Exception e) {
+        // print the error:
+        System.err.println(CLI.infile + " " + e);
+        scanner.consume();
+      }
+    }
+  }
+
+  private static void parse(InputStream inputStream, PrintStream outputStream) throws RecognitionException, TokenStreamException {
+    DecafScanner scanner = new DecafScanner(new DataInputStream(inputStream));
+    DecafParser parser = new DecafParser(scanner);
+    parser.setTrace(CLI.debug);
+    parser.program();
+    if(parser.getError()) {
+      System.exit(1);
     }
   }
 }
