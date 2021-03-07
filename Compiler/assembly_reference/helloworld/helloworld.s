b7d88e0a0329d632c27edbf14899f22508b48044
philoinovsky <1129410550@qq.com>
2021-03-07 07:27:20 +0000

    updated at Mar 7 2021

---
+++ Compiler/assembly_reference/helloworld/helloworld.s
@@ -0,0 +1,68 @@
+	.file	"helloworld.c"
+# GNU C17 (Ubuntu 8.4.0-1ubuntu1~18.04) version 8.4.0 (x86_64-linux-gnu)
+#	compiled by GNU C version 8.4.0, GMP version 6.1.2, MPFR version 4.0.1, MPC version 1.1.0, isl version isl-0.19-GMP
+
+# GGC heuristics: --param ggc-min-expand=97 --param ggc-min-heapsize=126103
+# options passed:  -imultiarch x86_64-linux-gnu helloworld.c -mtune=generic
+# -march=x86-64 -fverbose-asm -fstack-protector-strong -Wformat
+# -Wformat-security
+# options enabled:  -fPIC -fPIE -faggressive-loop-optimizations
+# -fasynchronous-unwind-tables -fauto-inc-dec -fchkp-check-incomplete-type
+# -fchkp-check-read -fchkp-check-write -fchkp-instrument-calls
+# -fchkp-narrow-bounds -fchkp-optimize -fchkp-store-bounds
+# -fchkp-use-static-bounds -fchkp-use-static-const-bounds
+# -fchkp-use-wrappers -fcommon -fdelete-null-pointer-checks
+# -fdwarf2-cfi-asm -fearly-inlining -feliminate-unused-debug-types
+# -ffp-int-builtin-inexact -ffunction-cse -fgcse-lm -fgnu-runtime
+# -fgnu-unique -fident -finline-atomics -fira-hoist-pressure
+# -fira-share-save-slots -fira-share-spill-slots -fivopts
+# -fkeep-static-consts -fleading-underscore -flifetime-dse
+# -flto-odr-type-merging -fmath-errno -fmerge-debug-strings -fpeephole
+# -fplt -fprefetch-loop-arrays -freg-struct-return
+# -fsched-critical-path-heuristic -fsched-dep-count-heuristic
+# -fsched-group-heuristic -fsched-interblock -fsched-last-insn-heuristic
+# -fsched-rank-heuristic -fsched-spec -fsched-spec-insn-heuristic
+# -fsched-stalled-insns-dep -fschedule-fusion -fsemantic-interposition
+# -fshow-column -fshrink-wrap-separate -fsigned-zeros
+# -fsplit-ivs-in-unroller -fssa-backprop -fstack-protector-strong
+# -fstdarg-opt -fstrict-volatile-bitfields -fsync-libcalls -ftrapping-math
+# -ftree-cselim -ftree-forwprop -ftree-loop-if-convert -ftree-loop-im
+# -ftree-loop-ivcanon -ftree-loop-optimize -ftree-parallelize-loops=
+# -ftree-phiprop -ftree-reassoc -ftree-scev-cprop -funit-at-a-time
+# -funwind-tables -fverbose-asm -fzero-initialized-in-bss
+# -m128bit-long-double -m64 -m80387 -malign-stringops
+# -mavx256-split-unaligned-load -mavx256-split-unaligned-store
+# -mfancy-math-387 -mfp-ret-in-387 -mfxsr -mglibc -mieee-fp
+# -mlong-double-80 -mmmx -mno-sse4 -mpush-args -mred-zone -msse -msse2
+# -mstv -mtls-direct-seg-refs -mvzeroupper
+
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello World"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp	#
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp	#,
+	.cfi_def_cfa_register 6
+# helloworld.c:5:     printf("Hello World");
+	leaq	.LC0(%rip), %rdi	#,
+	movl	$0, %eax	#,
+	call	printf@PLT	#
+# helloworld.c:6:     return 0;
+	movl	$0, %eax	#, _3
+# helloworld.c:7: }
+	popq	%rbp	#
+	.cfi_def_cfa 7, 8
+	ret	
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 8.4.0-1ubuntu1~18.04) 8.4.0"
+	.section	.note.GNU-stack,"",@progbits
