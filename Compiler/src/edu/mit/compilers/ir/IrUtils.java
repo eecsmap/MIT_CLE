1736fc365bf0a57e5c4149f7258eae21d5d06692
philoinovsky <1129410550@qq.com>
2021-04-18 03:47:25 +0800

    updated at Apr 18 2021

--- Compiler/src/edu/mit/compilers/ir/IrUtils.java
+++ Compiler/src/edu/mit/compilers/ir/IrUtils.java
@@ -260,29 +260,54 @@ public class IrUtils {
         return Defs.getArrayType(type);
     }
 
-    private static void parseIf(AST t, ST st) {
+    private static void parseIf(AST t, ST st, List<String> codes) {
         ST localST = new ST(st);
         AST c = t.getFirstChild();
-        String type = parseExpr(c, localST);
+        Label ifExecutionEndLabel = new Label();
+        Label ifElseEndLabel = new Label();
+        // condition
+        List<String> codesCondition = new ArrayList<>();
+        String type = parseExpr(c, localST, codesCondition);
+        Boolean hasElse = false;
         if (type != null && !Defs.equals(Defs.DESC_TYPE_BOOL, type)) {
             System.err.printf("14 ");
             Er.errType(c, Defs.DESC_TYPE_BOOL, type);
         }
-        c = parseBlock(c.getNextSibling(), localST);
-        if (c == null) {
-            return;
+        // if block
+        List<String> codesIfExecution = new ArrayList<>();
+        c = parseBlock(c.getNextSibling(), localST, codesIfExecution);
+        // else block
+        List<String> codesElseExecution = new ArrayList<>();
+        if (c != null) {
+            hasElse = true;
+            parseBlock(c.getFirstChild(), localST, codesElseExecution);
         }
-        // else
-        parseBlock(c.getFirstChild(), localST);
+        if (!Er.hasError()) {
+            codesCondition.add(
+                asm.jmp("je", ifExecutionEndLabel)
+            );
+            if (hasElse) 
+            codesIfExecution.add(
+                asm.jmp("jmp", ifElseEndLabel)
+            );
+            codes.addAll(codesCondition);
+            codes.addAll(codesIfExecution);
+            codes.add(asm.label(ifExecutionEndLabel));
+            if (hasElse)
+            {
+            codes.addAll(codesElseExecution);
+            codes.add(asm.label(ifElseEndLabel));
+            }
+        }
     }
 
     // doesn't suppeort declaration in for loop
     private static void parseFor(AST t, ST st, List<String> codes) {
         ST localST = new ST(st);
-        localST.pushContext(t.getType());
         Label executionBeginLabel = new Label();
         Label conditionBeginLabel = new Label();
         Label loopEndLabel = new Label();
+        localST.pushContext(t.getType());
         if (!Er.hasError()) {
             localST.pushContinueLabel(conditionBeginLabel);
             localST.pushBreakLabel(loopEndLabel);
@@ -328,18 +353,46 @@ public class IrUtils {
         }
     }
 
-    private static void parseWhile(AST t, ST st) {
+    private static void parseWhile(AST t, ST st, List<String> codes) {
         ST localST = new ST(st);
+        Label executionBeginLabel = new Label();
+        Label conditionBeginLabel = new Label();
+        Label loopEndLabel = new Label();
         localST.pushContext(t.getType());
+        if (!Er.hasError()) {
+            localST.pushContinueLabel(conditionBeginLabel);
+            localST.pushBreakLabel(loopEndLabel);
+        }
+        // condition
         AST c = t.getFirstChild();
-        String type = parseExpr(c, localST);
+        List<String> codesCondition = new ArrayList<>();
+        String type = parseExpr(c, localST, codesCondition);
         if (type != null && !Defs.equals(Defs.DESC_TYPE_BOOL, type)) {
             System.err.printf("15 ");
             Er.errType(c, Defs.DESC_TYPE_BOOL, type);
         }
+        // execution block
         c = c.getNextSibling();
-        parseBlock(c, localST);
+        List<String> codesExecution = new ArrayList<>();
+        parseBlock(c, localST, codesCondition);
+        if (!Er.hasError()) {
+            codesExecution.add(0,
+                asm.jmp("jmp", conditionBeginLabel)
+            );
+            codesCondition.add(
+                asm.jmp("jle", executionBeginLabel)
+            );
+            codes.add(asm.label(executionBeginLabel));
+            codes.addAll(codesExecution);
+            codes.add(asm.label(conditionBeginLabel));
+            codes.addAll(codesCondition);
+            codes.add(asm.label(loopEndLabel));
+        }
         localST.popContext();
+        if (!Er.hasError()) {
+            localST.popContinueLabel();
+            localST.popBreakLabel();
+        }
     }
 
     private static String parseIntLiteral(AST t, boolean isNegative) {
