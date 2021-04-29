b382607cc3bf2b78922403933931a3ef3dca5d61
philoinovsky <1129410550@qq.com>
2021-04-30 00:12:32 +0800

    updated at Apr 30 2021

---
+++ Compiler/src/edu/mit/compilers/compile/CompileElement.java
@@ -0,0 +1,59 @@
+package edu.mit.compilers.compile;
+
+import java.util.Collections;
+import java.util.List;
+
+import edu.mit.compilers.asm.Addr;
+import edu.mit.compilers.asm.Num;
+import edu.mit.compilers.asm.Oprand;
+import edu.mit.compilers.asm.Reg;
+import edu.mit.compilers.asm.asm;
+import edu.mit.compilers.asm.Action.ActionType;
+import edu.mit.compilers.defs.Defs;
+import edu.mit.compilers.st.ArrayDesc;
+import edu.mit.compilers.st.Descriptor;
+import edu.mit.compilers.st.ST;
+import edu.mit.compilers.syntax.Program;
+
+public class CompileElement {
+    public static void arrayElement(ST st, ActionType action, Descriptor desc, List<String> codes) {
+        if (!Program.shouldCompile()) {
+            return;
+        }
+        String varName = String.format("%s[]", desc.getText());
+        Oprand index = st.tmpPop();
+        Reg resReg = st.newTmpReg();
+        Reg indexReg = st.newTmpReg(resReg);
+        Integer offset = desc.getAddr().getOffset();
+        Collections.addAll(codes,
+            asm.bin("movq", index, indexReg),
+            asm.bin("cmpq", new Num(((ArrayDesc)desc).getCap()), indexReg),
+            asm.jmp("jge", Defs.EXIT_ARRAY_OUTBOUND_LABEL),
+            asm.bin("cmpq", new Num(0L), indexReg),
+            asm.jmp("jl", Defs.EXIT_ARRAY_OUTBOUND_LABEL)
+        );
+        if (desc.getAddr().isGlobal()) {
+            Collections.addAll(codes,
+                asm.bin("leaq", new Addr(indexReg, varName), indexReg),
+                asm.bin("leaq", desc.getAddr(), resReg)
+            );
+            if (action == ActionType.STORE) {
+                st.tmpPush(new Addr(indexReg, resReg));
+            } else {
+                codes.add(
+                    asm.bin("movq", new Addr(indexReg, resReg), resReg)
+                );
+                st.tmpPush(resReg);
+            }
+        } else {
+            if (action == ActionType.STORE) {
+                st.tmpPush(new Addr(offset, indexReg, varName));
+            } else {
+                codes.add(
+                    asm.bin("movq", new Addr(offset, indexReg, varName), resReg)
+                );
+                st.tmpPush(resReg);
+            }
+        }
+    }
+}
--- Compiler/src/edu/mit/compilers/syntax/Element.java
+++ Compiler/src/edu/mit/compilers/syntax/Element.java
@@ -1,17 +1,12 @@
 package edu.mit.compilers.syntax;
 
-import java.util.Collections;
 import java.util.List;
 
 import antlr.collections.AST;
-import edu.mit.compilers.asm.Addr;
 import edu.mit.compilers.asm.Num;
-import edu.mit.compilers.asm.Oprand;
-import edu.mit.compilers.asm.Reg;
-import edu.mit.compilers.asm.asm;
 import edu.mit.compilers.asm.Action.ActionType;
+import edu.mit.compilers.compile.CompileElement;
 import edu.mit.compilers.defs.Defs;
-import edu.mit.compilers.st.ArrayDesc;
 import edu.mit.compilers.st.Descriptor;
 import edu.mit.compilers.st.ST;
 import edu.mit.compilers.tools.Er;
@@ -36,43 +31,7 @@ public class Element {
             Er.errArrayIndexNotInt(t, Defs.DESC_TYPE_INT, indexType);
             return Defs.getArrayType(type);
         }
-        if (Program.shouldCompile()) {
-            String varName = String.format("%s[]", desc.getText());
-            Oprand index = st.tmpPop();
-            Reg resReg = st.newTmpReg();
-            Reg indexReg = st.newTmpReg(resReg);
-            Integer offset = desc.getAddr().getOffset();
-            Collections.addAll(codes,
-                asm.bin("movq", index, indexReg),
-                asm.bin("cmpq", new Num(((ArrayDesc)desc).getCap()), indexReg),
-                asm.jmp("jge", Defs.EXIT_ARRAY_OUTBOUND_LABEL),
-                asm.bin("cmpq", new Num(0L), indexReg),
-                asm.jmp("jl", Defs.EXIT_ARRAY_OUTBOUND_LABEL)
-            );
-            if (desc.getAddr().isGlobal()) {
-                Collections.addAll(codes,
-                    asm.bin("leaq", new Addr(indexReg, varName), indexReg),
-                    asm.bin("leaq", desc.getAddr(), resReg)
-                );
-                if (action == ActionType.STORE) {
-                    st.tmpPush(new Addr(indexReg, resReg));
-                } else {
-                    codes.add(
-                        asm.bin("movq", new Addr(indexReg, resReg), resReg)
-                    );
-                    st.tmpPush(resReg);
-                }
-            } else {
-                if (action == ActionType.STORE) {
-                    st.tmpPush(new Addr(offset, indexReg, varName));
-                } else {
-                    codes.add(
-                        asm.bin("movq", new Addr(offset, indexReg, varName), resReg)
-                    );
-                    st.tmpPush(resReg);
-                }
-            }
-        }
+        CompileElement.arrayElement(st, action, desc, codes);
         return Defs.getArrayType(type);
 
     }
@@ -94,9 +53,7 @@ public class Element {
                 Er.errIntegerTooLarge(t, number);
             }
         }
-        if (Program.shouldCompile()) {
-            st.tmpPush(new Num(result));
-        }
+        st.tmpPush(new Num(result));
         return Defs.DESC_TYPE_INT;
     }
 }
