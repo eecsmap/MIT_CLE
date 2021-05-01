e396036de200369b284253d17153add2449e8ef4
philoinovsky <1129410550@qq.com>
2021-05-02 02:04:08 +0800

    updated at May 2 2021

--- Compiler/src/edu/mit/compilers/ast/AstUtils.java
+++ Compiler/src/edu/mit/compilers/ast/AstUtils.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Collections;
 import antlr.collections.AST;
 
 import edu.mit.compilers.grammar.*;
@@ -12,101 +11,103 @@ import edu.mit.compilers.grammar.*;
 // for debug only
 public class AstUtils {
     // num to strignn
-    public static final Map<Integer, String> t0;
+    public static final Map<Integer, String> t0 = new HashMap<>() {{
+        put(4, "TK_bool");
+        put(5, "TK_break");
+        put(6, "TK_import");
+        put(7, "TK_continue");
+        put(8, "TK_else");
+        put(9, "TK_false");
+        put(10, "TK_for");
+        put(11, "TK_while");
+        put(12, "TK_if");
+        put(13, "TK_int");
+        put(14, "TK_return");
+        put(15, "TK_true");
+        put(16, "TK_void");
+        put(17, "TK_len");
+        put(18, "LCURLY");
+        put(19, "RCURLY");
+        put(20, "LSQUAR");
+        put(21, "RSQUAR");
+        put(22, "LPAREN");
+        put(23, "RPAREN");
+        put(24, "SEMICOLON");
+        put(25, "MINUS");
+        put(26, "PLUS");
+        put(27, "ASSIGN");
+        put(28, "TIMES");
+        put(29, "GREATER");
+        put(30, "LESS");
+        put(31, "GE");
+        put(32, "LE");
+        put(33, "EQ");
+        put(34, "NEQ");
+        put(35, "AND");
+        put(36, "OR");
+        put(37, "COMMA");
+        put(38, "DIGIT");
+        put(39, "LOWERCASE");
+        put(40, "UPPERCASE");
+        put(41, "HEXDIGIT");
+        put(42, "DEC");
+        put(43, "HEX");
+        put(44, "INTLITERAL");
+        put(45, "ESC");
+        put(46, "CHAR");
+        put(47, "CHARLITERAL");
+        put(48, "STRINGLITERAL");
+        put(49, "ALPHA");
+        put(50, "ID");
+        put(51, "WS_");
+        put(52, "SL_COMMENT");
+        put(53, "IL_COMMENT");
+        put(54, "QUESTION");
+        put(55, "SLASH");
+        put(56, "PERCENT");
+        put(57, "PLUSASSIGN");
+        put(58, "MINUSASSIGN");
+        put(59, "EXCLAM");
+        put(60, "COLON");
+        put(61, "INCRE");
+        put(62, "DECRE");
+    }};
     // binary ops (int -> int)
-    public static final Set<Integer> s0 = new HashSet<>();
+    public static final Set<Integer> s0 = new HashSet<>() {{
+        add(DecafParserTokenTypes.MINUS);
+        add(DecafParserTokenTypes.PLUS);
+        add(DecafParserTokenTypes.TIMES);
+        add(DecafParserTokenTypes.SLASH);
+        add(DecafParserTokenTypes.PERCENT);
+    }};
     // binary ops (int -> bool)
-    public static final Set<Integer> s1 = new HashSet<>();
+    public static final Set<Integer> s1 = new HashSet<>() {{
+        add(DecafParserTokenTypes.GREATER);
+        add(DecafParserTokenTypes.LESS);
+        add(DecafParserTokenTypes.GE);
+        add(DecafParserTokenTypes.LE);
+    }};
     // binary ops (bool -> bool)
-    public static final Set<Integer> s2 = new HashSet<>();
+    public static final Set<Integer> s2 = new HashSet<>() {{
+        add(DecafParserTokenTypes.AND);
+        add(DecafParserTokenTypes.OR);
+    }};
     // binary ops (assignment: =, +=, -=)
-    public static final Set<Integer> s3 = new HashSet<>();
+    public static final Set<Integer> s3 = new HashSet<>() {{
+        add(DecafParserTokenTypes.ASSIGN);
+        add(DecafParserTokenTypes.MINUSASSIGN);
+        add(DecafParserTokenTypes.PLUSASSIGN);
+    }};
     // unary ops (assignment: ++, --)
-    public static final Set<Integer> s4 = new HashSet<>();
+    public static final Set<Integer> s4 = new HashSet<>() {{
+        add(DecafParserTokenTypes.INCRE);
+        add(DecafParserTokenTypes.DECRE);
+    }};
     // binary ops (* -> bool)
-    public static final Set<Integer> s5 = new HashSet<>();
-
-    static {
-        Map<Integer, String> m1 = new HashMap<>();
-        m1.put(4, "TK_bool");
-        m1.put(5, "TK_break");
-        m1.put(6, "TK_import");
-        m1.put(7, "TK_continue");
-        m1.put(8, "TK_else");
-        m1.put(9, "TK_false");
-        m1.put(10, "TK_for");
-        m1.put(11, "TK_while");
-        m1.put(12, "TK_if");
-        m1.put(13, "TK_int");
-        m1.put(14, "TK_return");
-        m1.put(15, "TK_true");
-        m1.put(16, "TK_void");
-        m1.put(17, "TK_len");
-        m1.put(18, "LCURLY");
-        m1.put(19, "RCURLY");
-        m1.put(20, "LSQUAR");
-        m1.put(21, "RSQUAR");
-        m1.put(22, "LPAREN");
-        m1.put(23, "RPAREN");
-        m1.put(24, "SEMICOLON");
-        m1.put(25, "MINUS");
-        m1.put(26, "PLUS");
-        m1.put(27, "ASSIGN");
-        m1.put(28, "TIMES");
-        m1.put(29, "GREATER");
-        m1.put(30, "LESS");
-        m1.put(31, "GE");
-        m1.put(32, "LE");
-        m1.put(33, "EQ");
-        m1.put(34, "NEQ");
-        m1.put(35, "AND");
-        m1.put(36, "OR");
-        m1.put(37, "COMMA");
-        m1.put(38, "DIGIT");
-        m1.put(39, "LOWERCASE");
-        m1.put(40, "UPPERCASE");
-        m1.put(41, "HEXDIGIT");
-        m1.put(42, "DEC");
-        m1.put(43, "HEX");
-        m1.put(44, "INTLITERAL");
-        m1.put(45, "ESC");
-        m1.put(46, "CHAR");
-        m1.put(47, "CHARLITERAL");
-        m1.put(48, "STRINGLITERAL");
-        m1.put(49, "ALPHA");
-        m1.put(50, "ID");
-        m1.put(51, "WS_");
-        m1.put(52, "SL_COMMENT");
-        m1.put(53, "IL_COMMENT");
-        m1.put(54, "QUESTION");
-        m1.put(55, "SLASH");
-        m1.put(56, "PERCENT");
-        m1.put(57, "PLUSASSIGN");
-        m1.put(58, "MINUSASSIGN");
-        m1.put(59, "EXCLAM");
-        m1.put(60, "COLON");
-        m1.put(61, "INCRE");
-        m1.put(62, "DECRE");
-        t0 = Collections.unmodifiableMap(m1);
-        s0.add(DecafParserTokenTypes.MINUS);
-        s0.add(DecafParserTokenTypes.PLUS);
-        s0.add(DecafParserTokenTypes.TIMES);
-        s0.add(DecafParserTokenTypes.SLASH);
-        s0.add(DecafParserTokenTypes.PERCENT);
-        s1.add(DecafParserTokenTypes.GREATER);
-        s1.add(DecafParserTokenTypes.LESS);
-        s1.add(DecafParserTokenTypes.GE);
-        s1.add(DecafParserTokenTypes.LE);
-        s2.add(DecafParserTokenTypes.AND);
-        s2.add(DecafParserTokenTypes.OR);
-        s3.add(DecafParserTokenTypes.ASSIGN);
-        s3.add(DecafParserTokenTypes.MINUSASSIGN);
-        s3.add(DecafParserTokenTypes.PLUSASSIGN);
-        s4.add(DecafParserTokenTypes.INCRE);
-        s4.add(DecafParserTokenTypes.DECRE);
-        s5.add(DecafParserTokenTypes.EQ);
-        s5.add(DecafParserTokenTypes.NEQ);
-    }
+    public static final Set<Integer> s5 = new HashSet<>() {{
+        add(DecafParserTokenTypes.EQ);
+        add(DecafParserTokenTypes.NEQ);
+    }};
 
     public static void printAST(AST t, int level) {
         while(t != null){
