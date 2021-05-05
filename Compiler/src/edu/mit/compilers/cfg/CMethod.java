308cb42db1dd7fa19c63c3e410d56b8e5ada0b41
philoinovsky <1129410550@qq.com>
2021-05-06 00:31:05 +0800

    updated at May 6 2021

--- Compiler/src/edu/mit/compilers/cfg/CMethod.java
+++ Compiler/src/edu/mit/compilers/cfg/CMethod.java
@@ -2,8 +2,10 @@ package edu.mit.compilers.cfg;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import edu.mit.compilers.asm.AJmpLine;
 import edu.mit.compilers.asm.ALabelLine;
@@ -17,25 +19,30 @@ public class CMethod {
 
     public CMethod(AMethod aMethod) {
         int last = 0;
-        Map<CBlock, String> jmpMap = new HashMap<>();
-        Map<String, CBlock> labelMap = new HashMap<>();
+        Map<CBlock, String> jMap = new HashMap<>();
+        Map<String, CBlock> lMap = new HashMap<>();
+        Set<CBlock> blocksEndWithJmp = new HashSet<>();
         for (int i = 0; i < aMethod.size(); i++) {
             ALine line = aMethod.get(i);
             // condition jump
-            if (line instanceof AJmpLine && !((AJmpLine)line).getInst().equals("jmp")) {
+            if (line instanceof AJmpLine) {
                 Label label = ((AJmpLine)line).getLabel();
-                if (!label.equals(Defs.EXIT_ARRAY_OUTBOUND_LABEL)) {
-                    CBlock block = new CBlock(aMethod.subLines(last, i + 1));
-                    jmpMap.put(block, label.toString());
-                    this.blocks.add(block);
-                    last = i + 1;
+                if (label.equals(Defs.EXIT_ARRAY_OUTBOUND_LABEL)) {
+                    continue;
+                }
+                CBlock block = new CBlock(aMethod.subLines(last, i + 1));
+                jMap.put(block, label.toString());
+                this.blocks.add(block);
+                last = i + 1;
+                if (((AJmpLine)line).getInst().equals("jmp")) {
+                    blocksEndWithJmp.add(block);
                 }
             }
             // must be used
             if (line instanceof ALabelLine) {
                 Label label = ((ALabelLine)line).getLabel();
                 CBlock block = new CBlock(aMethod.subLines(last, i));
-                labelMap.put(label.toString(), block);
+                lMap.put(label.toString(), block);
                 this.blocks.add(block);
                 last = i;
             }
@@ -45,13 +52,15 @@ public class CMethod {
         this.blocks.get(0).addSucc(this.blocks.get(1));
         for (int i = 1; i < this.blocks.size() - 1; i++) {
             this.blocks.get(i).addPred(this.blocks.get(i-1));
-            this.blocks.get(i).addSucc(this.blocks.get(i+1));
+            if (!blocksEndWithJmp.contains(this.blocks.get(i))) {
+                this.blocks.get(i).addSucc(this.blocks.get(i+1));
+            }
         }
         this.blocks.get(this.blocks.size()-1).addPred(this.blocks.get(this.blocks.size()-2));
         // link to jmp
-        for (Map.Entry<CBlock, String> entry : jmpMap.entrySet()) {
+        for (Map.Entry<CBlock, String> entry : jMap.entrySet()) {
             CBlock from = entry.getKey();
-            CBlock to = labelMap.get(entry.getValue());
+            CBlock to = lMap.get(entry.getValue());
             from.addSucc(from);
             to.addSucc(from);
         }
