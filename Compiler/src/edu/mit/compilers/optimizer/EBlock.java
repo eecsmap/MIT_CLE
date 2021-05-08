2af591772ebe14f976ed639bd14ed66eddd0d3b6
philoinovsky <1129410550@qq.com>
2021-05-08 15:18:47 +0800

    updated at May 8 2021

--- Compiler/src/edu/mit/compilers/optimizer/EBlock.java
+++ Compiler/src/edu/mit/compilers/optimizer/EBlock.java
@@ -1,11 +1,19 @@
 package edu.mit.compilers.optimizer;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
 
 
 public class EBlock {
-    private TreeSet<Expr> set = new TreeSet<>();
+    private TreeSet<Expr> set = new TreeSet<Expr>(
+        new Comparator<Expr>() {
+            @Override
+            public int compare(Expr lhs, Expr rhs) {
+                return lhs.compare(rhs);
+            }
+        }
+    );
 
     public EBlock() {}
 
